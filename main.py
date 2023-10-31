417abf1f14997596ce44d85d5333836723e95e8e
Coufal, Jan <jan.coufal@gmail.com>
2023-10-31 15:49:24 +0100

    ProtoTile reworked

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import enum
 from enum import Enum, auto
 import tkinter as tk
+from dataclasses import dataclass
 import random
 from PIL import Image, ImageTk
 from pathlib import Path
@@ -12,54 +13,62 @@ IMAGE_EDGE_SIZE = (WINDOW_EDGE_SIZE // GRID_SIZE)
 
 
 class TileEdge(Enum):
-	UP = "y-",
-	RIGHT = "x+"
-	DOWN = "y+"
-	LEFT = "x-"
+	UP = 0,  # y-
+	RIGHT = 1,  # x+
+	DOWN = 2,  # y+
+	LEFT = 3,  # x-
 
 
-class ProtoTile(object):
-	def __init__(self, image: Image):
-		self._image = image
-		self._neighbor_map = {e: set() for e in TileEdge}
+class TileTransformation(Enum):
+	ROTATE_BY_90_DEG_ALL_AROUND = auto(),
+	# ROTATE_BY_180_DEG = auto(),
+	# MIRROR_HORIZONTALLY = auto(),
+	# MIRROR_VERTICALLY = auto(),
+	# ROTATE_BY_90_DEG_ALL_AROUND_MIRRORED = auto(),
+	# ROTATE_BY_180_DEG_MIRRORED = auto(),
 
-	# set[object] == set[ProtoTile]
-	def set_neighbor_map(self, neighbor_map: dict[TileEdge, set[object]]):
-		self._neighbor_map = neighbor_map
 
-	@property
-	def image(self):
-		return self._image
+@dataclass
+class TileEdges:
+	up: int
+	right: int
+	down: int
+	left: int
 
-	def get_neighbor_map(self, tile_edge: TileEdge):
-		return self._neighbor_map[tile_edge]
+	def make_rotate_left(self, amount: int):
+		# https://stackoverflow.com/questions/5299135/how-to-efficiently-left-shift-a-tuple
+		edges = (self.up, self.right, self.down, self.left)
+		amount = amount % len(edges)
+		return TileEdges(*(edges[amount:] + edges[0:amount]))
+
+
+@dataclass
+class ProtoTile:
+	image: Image
+	image_tk: ImageTk.PhotoImage
+	edges: TileEdges
+
+	@classmethod
+	def create(cls, image: Image, edges: TileEdges):
+		return ProtoTile(image=image, image_tk=ImageTk.PhotoImage(image), edges=edges)
 
 
 class TileFactory(object):
+	def __init__(self, image_dir: str, tile_image_size: int):
+		self._image_dir = Path(image_dir)
+		self._image_size = tile_image_size
 
-	@classmethod
-	def create_5_set(cls, image_dir: Path, image_size: int):
-		def gen_tile(_type):
-			return ProtoTile(Image.open(image_dir / (_type + ".png")).resize((image_size, image_size)))
+	def generate_tiles(self, image_name: str, edges: TileEdges, transformations: tuple[TileTransformation] = None) -> tuple[ProtoTile]:
+		image = Image.open(self._image_dir / image_name).resize((self._image_size, self._image_size))
+		tiles = []
 
-		b = gen_tile("blank")
-		u = gen_tile("up")
-		r = gen_tile("right")
-		d = gen_tile("down")
-		l = gen_tile("left")
+		for i in range(1, 4):
+			tiles.append(ProtoTile.create(image.rotate(i * 90), edges.make_rotate_left(i)))
 
-		# define neighbors
-		b.set_neighbor_map({
-			TileEdge.UP: (b, u),
-			TileEdge.RIGHT: (b, r),
-			TileEdge.DOWN: (b, d),
-			TileEdge.LEFT: (b, l),
-		})
-
-	def __init__(self, tiles: set):
-		self._tiles = tiles
+		return tuple(tiles)
 
 
+"""
 class Tile(object):
 	def __init__(self, options: set):
 		self._options = set(options)
@@ -78,14 +87,17 @@ class Tile(object):
 
 	def get_collapsed_option(self):
 		return tuple(self._options)[0] if self.is_collapsed() else None
-
+"""
 
 # class TileBoard(object):
+
 
 class TkApp(tk.Tk):
 	def __init__(self):
 		super().__init__()
 		print("init")
+
+		self._tile_factory = TileFactory("img/mountains", IMAGE_EDGE_SIZE)
 
 		self.title("Wave function collapse test (or Wang tiles?)")
 		self.canvas = tk.Canvas(
@@ -96,18 +108,22 @@ class TkApp(tk.Tk):
 		self.canvas.pack()
 		# self.geometry(f"{WINDOW_EDGE_SIZE + WINDOW_PADDING}x{WINDOW_EDGE_SIZE + WINDOW_PADDING}")
 
-		self.tk_images = {
-			TileType.BLANK: ImageTk.PhotoImage(Image.open("img/mountains/blank.png").resize((IMAGE_EDGE_SIZE, IMAGE_EDGE_SIZE))),
-			TileType.UP: ImageTk.PhotoImage(Image.open("img/mountains/up.png").resize((IMAGE_EDGE_SIZE, IMAGE_EDGE_SIZE))),
-			TileType.RIGHT: ImageTk.PhotoImage(Image.open("img/mountains/right.png").resize((IMAGE_EDGE_SIZE, IMAGE_EDGE_SIZE))),
-			TileType.DOWN: ImageTk.PhotoImage(Image.open("img/mountains/down.png").resize((IMAGE_EDGE_SIZE, IMAGE_EDGE_SIZE))),
-			TileType.LEFT: ImageTk.PhotoImage(Image.open("img/mountains/left.png").resize((IMAGE_EDGE_SIZE, IMAGE_EDGE_SIZE))),
-		}
+		self.tiles = self._tile_factory.generate_tiles("down.png", TileEdges(0, 1, 1, 1))
+
+		# self.tk_images = {
+		# 	TileType.BLANK: ImageTk.PhotoImage(Image.open("img/mountains/blank.png").resize((IMAGE_EDGE_SIZE, IMAGE_EDGE_SIZE))),
+		# 	TileType.UP: ImageTk.PhotoImage(Image.open("img/mountains/up.png").resize((IMAGE_EDGE_SIZE, IMAGE_EDGE_SIZE))),
+		# 	TileType.RIGHT: ImageTk.PhotoImage(Image.open("img/mountains/right.png").resize((IMAGE_EDGE_SIZE, IMAGE_EDGE_SIZE))),
+		# 	TileType.DOWN: ImageTk.PhotoImage(Image.open("img/mountains/down.png").resize((IMAGE_EDGE_SIZE, IMAGE_EDGE_SIZE))),
+		# 	TileType.LEFT: ImageTk.PhotoImage(Image.open("img/mountains/left.png").resize((IMAGE_EDGE_SIZE, IMAGE_EDGE_SIZE))),
+		# }
 
 		# self.frame = tk.Frame()
 		# self.frame.pack(expand=True)
 
-		self.canvas.create_image(IMAGE_EDGE_SIZE, 0, anchor=tk.NW, image=self.tk_images.get(TileType.BLANK))
+		for i, tile in enumerate(self.tiles):
+			print(f"{tile=}")
+			self.canvas.create_image(i*(IMAGE_EDGE_SIZE + WINDOW_PADDING), 0, anchor=tk.NW, image=tile.image_tk)
 		# self.canvas.pack()
 
 		# for i in range(GRID_SIZE * GRID_SIZE):
