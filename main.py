3ac8056634177b37c1a21b47b3fe529f1d9613f7
Coufal, Jan <jan.coufal@gmail.com>
2023-11-01 22:36:42 +0100

    total rework, wip

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
-import enum
-from enum import Enum, auto
-import tkinter as tk
-from dataclasses import dataclass
-import random
-from PIL import Image, ImageTk
-from pathlib import Path
+import typing
 
-GRID_SIZE = 8
+import math
+import random
+import tkinter as tk
+import operator
+from enum import Enum, auto
+from typing import Tuple, Optional
+from dataclasses import dataclass
+from copy import deepcopy
+from collections import defaultdict
+from pathlib import Path
+from PIL import Image, ImageTk
+
+GRID_SIZE = 3
 WINDOW_EDGE_SIZE = 512
 WINDOW_PADDING = 8
 IMAGE_EDGE_SIZE = (WINDOW_EDGE_SIZE // GRID_SIZE)
+
+
+@dataclass
+class Vec2:
+	x: int
+	y: int
+
+	def add(self, p):
+		return Vec2(self.x + p.x, self.y + p.y)
+
+	def mul(self, f):
+		return Vec2(self.x * f, self.y * f)
+
+	def as_tuple(self):
+		return self.x, self.y
+
+	def __str__(self):
+		return f"[{self.x}, {self.y}]"
 
 
 class TileEdge(Enum):
@@ -21,11 +45,11 @@ class TileEdge(Enum):
 
 class TileTransformation(Enum):
 	ROTATE_BY_90_DEG_ALL_AROUND = auto(),
-	# ROTATE_BY_180_DEG = auto(),
-	# MIRROR_HORIZONTALLY = auto(),
-	# MIRROR_VERTICALLY = auto(),
-	# ROTATE_BY_90_DEG_ALL_AROUND_MIRRORED = auto(),
-	# ROTATE_BY_180_DEG_MIRRORED = auto(),
+# ROTATE_BY_180_DEG = auto(),
+# MIRROR_HORIZONTALLY = auto(),
+# MIRROR_VERTICALLY = auto(),
+# ROTATE_BY_90_DEG_ALL_AROUND_MIRRORED = auto(),
+# ROTATE_BY_180_DEG_MIRRORED = auto(),
 
 
 @dataclass
@@ -41,16 +65,36 @@ class TileEdges:
 		amount = amount % len(edges)
 		return TileEdges(*(edges[amount:] + edges[0:amount]))
 
+	def __str__(self):
+		return f"\u21bb: {self.up, self.right, self.down, self.left}"
+
 
 @dataclass
 class ProtoTile:
 	image: Image
 	image_tk: ImageTk.PhotoImage
+	image_tk_mini: ImageTk.PhotoImage
 	edges: TileEdges
 
 	@classmethod
 	def create(cls, image: Image, edges: TileEdges):
-		return ProtoTile(image=image, image_tk=ImageTk.PhotoImage(image), edges=edges)
+		return ProtoTile(
+			image=image,
+			image_tk=ImageTk.PhotoImage(image),
+			image_tk_mini=ImageTk.PhotoImage(image.resize((IMAGE_EDGE_SIZE // 2, IMAGE_EDGE_SIZE // 2))),
+			edges=edges
+		)
+
+	def make_copy(self):
+		return ProtoTile(
+			image=self.image,
+			image_tk=self.image_tk,
+			image_tk_mini=self.image_tk_mini,
+			edges=deepcopy(self.edges)
+		)
+
+	def __str__(self):
+		return f"{self.edges}"
 
 
 class TileFactory(object):
@@ -58,97 +102,142 @@ class TileFactory(object):
 		self._image_dir = Path(image_dir)
 		self._image_size = tile_image_size
 
-	def generate_tiles(self, image_name: str, edges: TileEdges, transformations: tuple[TileTransformation] = None) -> tuple[ProtoTile]:
+	def generate_tiles(self, image_name: str, edges: TileEdges, transformations: tuple[TileTransformation] = None) -> Tuple[ProtoTile, ...]:
 		image = Image.open(self._image_dir / image_name).resize((self._image_size, self._image_size))
 		tiles = []
 
-		for i in range(1, 4):
+		for i in range(4):
 			tiles.append(ProtoTile.create(image.rotate(i * 90), edges.make_rotate_left(i)))
 
 		return tuple(tiles)
 
 
-"""
-class Tile(object):
-	def __init__(self, options: set):
-		self._options = set(options)
+@dataclass
+class TileState:
+	position: Vec2
+	available_tiles: list[ProtoTile]
+	final_tile: Optional[ProtoTile]
 
-	def is_collapsed(self):
-		return len(self._options) == 1
+	@classmethod
+	def create(cls, position: Vec2, initial_tiles: Tuple[ProtoTile, ...]):
+		return TileState(position=position, available_tiles=[t.make_copy() for t in initial_tiles], final_tile=None)
 
-	def is_out_of_options(self):
-		return len(self._options) == 0
+	def do_collapse(self):
+		self.final_tile = random.choice(self.available_tiles)
+		if self.final_tile is None:
+			raise RuntimeError(f"Failed to collapse tile {self!s}.")
+		self.available_tiles = []
 
-	def remove_options(self, options_to_remove: set):
-		self._options = self._options - set(options_to_remove)
+	def __str__(self):
+		return f"\u2316: {self.position}, #: {len(self.available_tiles)}, collapsed: {'y' if self.final_tile is not None else 'n'}"
 
-	def get_options(self) -> set:
-		return self._options
 
-	def get_collapsed_option(self):
-		return tuple(self._options)[0] if self.is_collapsed() else None
-"""
+class TileBoard(object):
 
-# class TileBoard(object):
+	_POSITION_MOVE_UNSAFE = {
+		TileEdge.UP: Vec2(-1, 0),
+		TileEdge.RIGHT: Vec2(0, +1),
+		TileEdge.DOWN: Vec2(+1, 0),
+		TileEdge.LEFT: Vec2(0, -1),
+	}
+
+	def __init__(self, board_size: Vec2, tiles: Tuple[ProtoTile, ...]):
+		self._board_size = board_size
+		self._proto_tiles = tiles
+		self._board = []
+
+	def build(self):
+		board_tile_count = self._board_size.x * self._board_size.y
+		self._board = [TileState.create(Vec2(*divmod(i, self._board_size.x)), self._proto_tiles) for i in range(board_tile_count)]
+
+		complete = False
+		steps_count = 1
+		while not complete:
+			least_available = self._find_tiles_with_least_available_tiles()
+
+			if len(least_available) == 0:
+				raise RuntimeError("No tiles found!")
+
+			picked_tile = random.choice(least_available)
+			picked_tile = self._get_tile_on_position_safe(Vec2(1,1))
+			picked_tile.do_collapse()
+
+			# update neighbor tiles
+			neighbors = {tileEdge: self._get_tile_in_direction(picked_tile, tileEdge) for tileEdge in TileEdge}
+			print(neighbors)
+
+			# TODO: update neighbor's tiles available_tiles
+
+			steps_count -= 1
+			complete = steps_count <= 0
+		print(self._board)
+
+	def _find_tiles_with_least_available_tiles(self) -> list[TileState]:
+		histogram = defaultdict(list)
+		for tile in self._board:
+			histogram[len(tile.available_tiles)].append(tile)
+		return histogram[min(histogram.keys())]
+
+	def _get_tile_in_direction(self, pivot_tile: TileState, direction: TileEdge) -> Optional[TileState]:
+		target_position = pivot_tile.position.add(TileBoard._POSITION_MOVE_UNSAFE[direction])
+		return self._get_tile_on_position_safe(target_position)
+
+	def _get_tile_on_position_safe(self, position: Vec2) -> Optional[TileState]:
+		return self._board[position.y * self._board_size.x + position.x] \
+			if 0 <= position.x < self._board_size.x and 0 <= position.y < self._board_size.y \
+			else None
+
+	def get_tiles(self) -> list[TileState]:
+		return self._board
 
 
 class TkApp(tk.Tk):
 	def __init__(self):
 		super().__init__()
-		print("init")
-
-		self._tile_factory = TileFactory("img/mountains", IMAGE_EDGE_SIZE)
 
 		self.title("Wave function collapse test (or Wang tiles?)")
 		self.canvas = tk.Canvas(
 			self,
-			width=(WINDOW_EDGE_SIZE + 0*WINDOW_PADDING),
-			height=(WINDOW_EDGE_SIZE + 0*WINDOW_PADDING)
+			width=(WINDOW_EDGE_SIZE + 0 * WINDOW_PADDING),
+			height=(WINDOW_EDGE_SIZE + 0 * WINDOW_PADDING)
 		)
 		self.canvas.pack()
 		# self.geometry(f"{WINDOW_EDGE_SIZE + WINDOW_PADDING}x{WINDOW_EDGE_SIZE + WINDOW_PADDING}")
 
+		self._tile_factory = TileFactory("img/mountains", IMAGE_EDGE_SIZE)
 		self.tiles = self._tile_factory.generate_tiles("down.png", TileEdges(0, 1, 1, 1))
 
-		# self.tk_images = {
-		# 	TileType.BLANK: ImageTk.PhotoImage(Image.open("img/mountains/blank.png").resize((IMAGE_EDGE_SIZE, IMAGE_EDGE_SIZE))),
-		# 	TileType.UP: ImageTk.PhotoImage(Image.open("img/mountains/up.png").resize((IMAGE_EDGE_SIZE, IMAGE_EDGE_SIZE))),
-		# 	TileType.RIGHT: ImageTk.PhotoImage(Image.open("img/mountains/right.png").resize((IMAGE_EDGE_SIZE, IMAGE_EDGE_SIZE))),
-		# 	TileType.DOWN: ImageTk.PhotoImage(Image.open("img/mountains/down.png").resize((IMAGE_EDGE_SIZE, IMAGE_EDGE_SIZE))),
-		# 	TileType.LEFT: ImageTk.PhotoImage(Image.open("img/mountains/left.png").resize((IMAGE_EDGE_SIZE, IMAGE_EDGE_SIZE))),
-		# }
-
-		# self.frame = tk.Frame()
-		# self.frame.pack(expand=True)
-
-		for i, tile in enumerate(self.tiles):
-			print(f"{tile=}")
-			self.canvas.create_image(i*(IMAGE_EDGE_SIZE + WINDOW_PADDING), 0, anchor=tk.NW, image=tile.image_tk)
-		# self.canvas.pack()
-
-		# for i in range(GRID_SIZE * GRID_SIZE):
-		# 	r, c = divmod(i, GRID_SIZE)
-		# 	self.canvas.create_image(
-		# 		r * IMAGE_EDGE_SIZE + WINDOW_PADDING,
-		# 		c * IMAGE_EDGE_SIZE + WINDOW_PADDING,
-		# 		image=self.tk_images.get(random.choice(list(TileType)))
-		# 	)
-
+		self.board = TileBoard(Vec2(GRID_SIZE, GRID_SIZE), self.tiles)
+		self.build_board()
 		self.update()
+
+	def build_board(self):
+		self.board.build()
 
 	def update(self):
 		super().update()
-		print("redraw")
 
-
-class Tile(object):
-	def __init__(self):
-		pass
-
-
-class TileGrid(object):
-	def __init__(self):
-		pass
+		for tile in self.board.get_tiles():
+			if tile.final_tile is not None:
+				self.canvas.create_image(
+					*tile.position.mul(IMAGE_EDGE_SIZE).as_tuple(),
+					anchor=tk.NW,
+					image=tile.final_tile.image_tk
+				)
+			else:
+				# draw first 4 available tiles
+				pos_delta = Vec2(IMAGE_EDGE_SIZE, IMAGE_EDGE_SIZE).mul(0.5)
+				for i, available_tile in enumerate(tile.available_tiles[:4]):
+					r, c = divmod(i, 2)
+					p = Vec2(
+						tile.position.x * IMAGE_EDGE_SIZE + c * pos_delta.x,
+						tile.position.y * IMAGE_EDGE_SIZE + r * pos_delta.y
+					)
+					self.canvas.create_image(
+						*p.as_tuple(),
+						anchor=tk.NW,
+						image=available_tile.image_tk_mini
+					)
 
 
 if __name__ == "__main__":
